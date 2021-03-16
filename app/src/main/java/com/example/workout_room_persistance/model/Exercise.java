81b7af1177e0bf67f7484de6bdbbf9788dcdb1ad
CyloTM <samuelchyke@hotmail.com>
2021-03-16 13:13:27 +0000

    Setting up Models

---
+++ app/src/main/java/com/example/workout_room_persistance/model/Exercise.java
@@ -0,0 +1,84 @@
+package com.example.workout_room_persistance.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "exercises")
+public class Exercise implements Parcelable {
+
+    //Fields
+    //Primary Key
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "repetitions")
+    private int repetitions;
+
+    public Exercise(@NonNull String title, int repetitions) {
+        this.title = title;
+        this.repetitions = repetitions;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NonNull String title) {
+        this.title = title;
+    }
+
+    public int getRepetitions() {
+        return repetitions;
+    }
+
+    public void setRepetitions(int repetitions) {
+        this.repetitions = repetitions;
+    }
+
+
+    protected Exercise(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        repetitions = in.readInt();
+    }
+
+    public static final Creator<Exercise> CREATOR = new Creator<Exercise>() {
+        @Override
+        public Exercise createFromParcel(Parcel in) {
+            return new Exercise(in);
+        }
+
+        @Override
+        public Exercise[] newArray(int size) {
+            return new Exercise[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+}
---
+++ app/src/main/java/com/example/workout_room_persistance/model/Workout.java
@@ -0,0 +1,104 @@
+package com.example.workout_room_persistance.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+import java.util.ArrayList;
+
+@Entity(tableName = "workouts")
+public class Workout implements Parcelable {
+
+    //Fields
+    //Primary Key
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "content")
+    private ArrayList<Exercise> exercises;
+
+
+    public Workout(String title, ArrayList<Exercise> exercises) {
+
+        this.title = title;
+        this.exercises = exercises;
+    }
+
+    @Ignore
+    public Workout(){
+
+    }
+
+    protected Workout(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        exercises = (ArrayList<Exercise>) in.readValue(Exercise.class.getClassLoader());
+    }
+
+    public static final Creator<Workout> CREATOR = new Creator<Workout>() {
+        @Override
+        public Workout createFromParcel(Parcel in) {
+            return new Workout(in);
+        }
+
+        @Override
+        public Workout[] newArray(int size) {
+            return new Workout[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(ArrayList<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id =" + id +
+                "title='" + title + '\'' +
+                ", exercises='" + exercises + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(title);
+        dest.writeValue(exercises);
+    }
+}
